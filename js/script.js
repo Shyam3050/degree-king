1404dff814b0c18aa5cde60bc8db6df9c8bfdd8b
shyam3050 <nimmanashyam3050@gmail.com>
2022-06-13 11:58:00 +0530

    U

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ const nav_btn = getElement(".nav-toggle-btn");
 nav_btn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
+// cross close for semester
+const cross = document.querySelectorAll(".sub-cross");
+const explore = document.querySelector(".subject");
+const check = getElement(".check");
+// console.log(cross);
+cross.forEach((cross) => {
+  cross.addEventListener("click", () => {
+    console.log(cross);
+  });
+});
+// explore.classList.add("active");
+check.addEventListener("click", () => {
+  explore.classList.add("active");
+});
